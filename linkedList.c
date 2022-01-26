1ede21b7668deccbf07d102b6c399761f1b6fc12
anurag <pothaluranurag2002@gmail.com>
2022-01-26 23:56:25 +0530

    queue using linked list added

--- linkedList.c
+++ linkedList.c
@@ -7,12 +7,15 @@ struct node{
 };
 void insertBeg();
 void display();
+void deleteBeg();
+void insertEnd();
+void deleteEnd();
 struct node *head;
 int main(){
     int choice, run=1;
     while(run==1){
         printf("\n----------OPTIONS----------------\n");
-        printf("1.Insert at the begining\n2.Display\n3.Insert at end\n");
+        printf("1.Insert at the begining\n2.Display\n3.Insert at end\n4.Delete from begining\n5.Delete from end\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
         switch (choice){
@@ -24,6 +27,12 @@ int main(){
             break;
         case 3:
             insertEnd();
+            break;
+        case 4:
+            deleteBeg();
+            break;
+        case 5:
+            deleteEnd();
             break;
         default:
             run=0;
@@ -53,5 +62,55 @@ void display(){
             printf("--->");
         }
         ptr = ptr->next;
+    }
+}
+void deleteBeg(){
+    struct node *ptr;
+    if(head==NULL){
+        printf("No elements left");
+    }else{
+        ptr = head;
+        head = ptr->next;
+        free(ptr);
+    }
+}
+void insertEnd(){
+    struct node *ptr, *temp;
+    int item;
+    ptr = (struct node *)malloc(sizeof(struct node *));
+    if(ptr==NULL){
+        printf("Overflow");
+    }else{
+        printf("Enter element: ");
+        scanf("%d", &item);
+        ptr->data = item;
+        if(head==NULL){
+            ptr->next = NULL;
+            head = ptr;
+        }else{
+            temp = head;
+            while(temp->next!=NULL){
+                temp = temp->next;
+            }
+            temp->next = ptr;
+            ptr->next = NULL;
+        }
+    }
+}
+void deleteEnd(){
+    struct node *ptr, *temp;
+    if(head==NULL){
+        printf("No elements");
+    }else if(head->next == NULL){
+        head = NULL;
+        free(head);
+    }else{
+        ptr = head;
+        while(ptr->next!=NULL){
+            temp = ptr;
+            ptr = ptr->next;
+        }
+        temp->next = NULL;
+        free(ptr);
     }
 }
