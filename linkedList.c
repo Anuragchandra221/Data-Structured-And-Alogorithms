8d7840a632a3370884f4347d9f4bd3b1a524fe0c
anurag <pothaluranurag2002@gmail.com>
2022-02-15 08:26:50 +0530

    Linked list finished

--- linkedList.c
+++ linkedList.c
@@ -10,12 +10,15 @@ void display();
 void deleteBeg();
 void insertEnd();
 void deleteEnd();
+void insertRandom();
+void deleteRandom();
+void search();
 struct node *head;
 int main(){
     int choice, run=1;
     while(run==1){
         printf("\n----------OPTIONS----------------\n");
-        printf("1.Insert at the begining\n2.Display\n3.Insert at end\n4.Delete from begining\n5.Delete from end\n");
+        printf("1.Insert at the begining\n2.Insert at end\n3.Insert in a given location\n4.Delete from begining\n5.Delete from end\n6.Delete from a given position\n7.Search\n8.Display\n9.Exit\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
         switch (choice){
@@ -23,10 +26,10 @@ int main(){
             insertBeg();
             break;
         case 2:
-            display();
+            insertEnd();
             break;
         case 3:
-            insertEnd();
+            insertRandom();
             break;
         case 4:
             deleteBeg();
@@ -34,8 +37,20 @@ int main(){
         case 5:
             deleteEnd();
             break;
+        case 6:
+            deleteRandom();
+            break;
+        case 7:
+            search();
+            break;
+        case 8:
+            display();
+            break;
+        case 9:
+            run = 0;
+            break;
         default:
-            run=0;
+            printf("Invalid input");
         }
     }
 }
@@ -108,6 +123,33 @@ void insertEnd(){
         }
     }
 }
+void insertRandom(){
+    struct node *ptr;
+    int item, key;
+    ptr = (struct node *)malloc(sizeof(struct node *));
+    if(ptr == NULL){
+        printf("Overflow");
+    }else{
+        printf("Enter element to be searched: ");
+        scanf("%d", &item);
+        ptr->data = item;
+        if(head!=NULL){
+            printf("Enter the key after which element has to be inserted: ");
+            scanf("%d", &key);
+            struct node *temp, *temp1;
+            temp = head;
+            while(temp->data != key){
+                temp = temp->next;
+            }
+            temp1 = temp->next;
+            temp->next = ptr;
+            ptr->next = temp1;
+        }else{
+            head = ptr;
+            ptr->next = NULL;
+        }
+    }
+}
 void deleteEnd(){
     struct node *ptr, *temp;
     if(head==NULL){
@@ -123,5 +165,45 @@ void deleteEnd(){
         }
         temp->next = NULL;
         free(ptr);
+    }
+}
+void deleteRandom(){
+    if(head!=NULL){
+        struct node *ptr, *temp;
+        ptr = head;
+        int key;
+        printf("Enter the key after which element has to be deleted: ");
+        scanf("%d", &key);
+        while(ptr->data != key){
+            ptr = ptr->next;
+        }
+        if(ptr->data == key && ptr->next == NULL){
+            printf("No next element");
+        }else{
+            temp = ptr->next;
+            ptr->next = temp->next;
+            free(temp);
+        }
+    }else{
+        printf("No elements in linked list");
+    }
+}
+void search(){
+    if(head!=NULL){
+        int item;
+        printf("Enter the element to be searched: ");
+        scanf("%d", &item);
+        struct node *ptr;
+        ptr = head;
+        while(ptr->data != item && ptr->next != NULL){
+            ptr = ptr->next;
+        }
+        if(ptr->data == item){
+            printf("Element found");
+        }else{
+            printf("Element not found");
+        }
+    }else{
+        printf("Linked list is empty");
     }
 }
